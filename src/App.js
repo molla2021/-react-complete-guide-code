2c7924310e8423060f65b5cd9c020018bc9263ff
molla2021 <moget994@gmail.com>
2021-12-28 11:46:14 +0300

    lifting the state up

--- src/App.js
+++ src/App.js
@@ -24,9 +24,15 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = expense =>{
+    console.log('In app.js');
+    console.log(expense);
+
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
