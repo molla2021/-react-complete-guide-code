d2bdeec562e42ae3ac46fc04524ba0a11a7a645b
molla2021 <moget994@gmail.com>
2021-12-30 23:29:30 +0300

    finshed code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import Expenses from './components/Expenses/Expenses';
+
 import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
   {
@@ -9,7 +10,10 @@ const DUMMY_EXPENSES = [
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', 
+    title: 'New TV', 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12) },
   {
     id: 'e3',
     title: 'Car Insurance',
@@ -24,22 +28,20 @@ const DUMMY_EXPENSES = [
   },
 ];
 
-function App() {
+const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-  
-  const addExpenseHandler = (expense) =>{
-    setExpenses((pervExpenses) =>{
-      return [expense, ...pervExpenses]
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
     });
-
   };
-
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items= {expenses} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
